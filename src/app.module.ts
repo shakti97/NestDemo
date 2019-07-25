992c15f386eefe2e46d1f6941011a2cd627d8673
shakti97 <shaktisingh230797@gmail.com>
2019-07-26 01:33:26 +0530

    Added Schema for Items

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,17 @@
-  import { Module } from '@nestjs/common';
-  import { AppController } from './app.controller';
-  import { AppService } from './app.service';
-  import { ItemsController } from './items/items.controller';
-  import { ItemsService } from './items/items.service';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ItemsController } from './items/items.controller';
+import { ItemsService } from './items/items.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ItemsModule } from './items/items.module';
 
-  @Module({
-    imports: [],
-    controllers: [AppController, ItemsController],
-    providers: [AppService, ItemsService],
-  })
-  export class AppModule {}
+//The forRoot() method accepts the same configuration object as mongoose.connect() from the Mongoose package.
+
+
+@Module({
+  imports: [ItemsModule,MongooseModule.forRoot('mongodb://localhost/nest')],
+  controllers: [AppController, ItemsController],
+  providers: [AppService, ItemsService],
+})
+export class AppModule {}
